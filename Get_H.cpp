f86e83b3dabd19be9a2be9af063f9e6edd504e49
StupidRabbit29 <2922282053@qq.com>
2019-05-31 00:32:36 +0800

    !!!!!!

--- Get_H.cpp
+++ Get_H.cpp
@@ -4,3 +4,11 @@ bool Get_Header(DNSheader& header, const char* src)
 {
 
 }
+
+
+//将报文src中的query字段中的查询域名拷贝到dest中，同时返回QTYPE
+QUERY_KIND Get_Query(char* dest, char* src)
+{
+	char* query = src + 12;
+
+}
--- main.h
+++ main.h
@@ -37,6 +37,6 @@ typedef struct Waiting
 //将报文src中的header字段拷贝到dest中，返回函数是否执行成功
 bool Get_Header(DNSheader& header, const char* src);
 //将报文src中的query字段中的查询域名拷贝到dest中，同时返回QTYPE
-QUERY_KIND Get_Query(char* dest, const char* src);
+QUERY_KIND Get_Query(char* dest, char* src);
 //在表中查找域名name， 将找到的IP地址存入IP，返回查找结果
 SEARCH_RESULT Serach(const char* name, char* IP);
