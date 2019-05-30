4720dda880ef62f7b94535029ef898ba078ebfc3
牧峰 王 <wmf.0317@qq.com>
2019-05-31 00:37:18 +0800

    write sub_fuction: Get_Header

--- Get_H.cpp
+++ Get_H.cpp
@@ -2,5 +2,17 @@
 
 bool Get_Header(DNSheader& header, const char* src)
 {
+	unsigned short tmp;			// 
 
 }
+
+/*
+//
+// src中的header部分共有六个部分，共12个字节: 
+// 1）ID: src[0]-src[1]: 2bytes
+// 2) Flags: src[2]-src[3]: 2bytes
+// 3) Questions: src[4]-src[5]: 2bytes
+// 4) Answer RRs: src[6]-src[7]: 2bytes
+// 5) Authority RRs: src[8]-src[9]: 2bytes
+// 6) Additional RRs: src[10]-src[11]: 2bytes
+*/
